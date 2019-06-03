3e81af3d11d2abebeaa276667d8ff5420c81b3fd
First-oboe <51189687+First-oboe@users.noreply.github.com>
2019-06-03 12:08:57 +0800

    a
    a

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     // 如果需要分页器
     pagination: '.swiper-pagination',
+    paginationClickable :true,
     autoplay:2500,
     autoplayDisableOnInteraction : false,
 })        
