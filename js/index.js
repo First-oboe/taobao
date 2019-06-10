2d260474734bd2d94be57e92fc770e3e669a0e35
First-oboe <51189687+First-oboe@users.noreply.github.com>
2019-06-10 12:01:32 +0800

    Revert "a"
    This reverts commit 3e81af3d11d2abebeaa276667d8ff5420c81b3fd.

--- js/index.js
+++ js/index.js
@@ -27,7 +27,6 @@ var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
     // 如果需要分页器
     pagination: '.swiper-pagination',
-    paginationClickable :true,
     autoplay:2500,
     autoplayDisableOnInteraction : false,
 })        
